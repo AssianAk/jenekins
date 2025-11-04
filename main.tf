41096ea951eebf48a0aab804ae78a2cfc74d78d7
AssianAk <107809552+AssianAk@users.noreply.github.com>
2025-11-05 00:09:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
   region = "eu-west-2"
 }
-resource "aws_s3_bucket" "name" {
-    bucket = "myawsnewbucket85521883094"
-  
+resource "aws_instance" "name" {
+    ami = "ami-06a8ca19af7f6f3f4"
+    key_name = "dev2"
+    instance_type = "t2.micro"
+    tags = {
+        Name=dev
+    }
 }
